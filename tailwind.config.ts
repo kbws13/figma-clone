a895bc038fb0b37ba37e37303636b70d61d91027
kbws13 <hsy040506@163.com>
2025-06-15 13:27:42 +0800

    feat: 添加 ESLint、Prettier 配置和对齐图标
    - 新增 .eslintrc.json 和 .prettierrc.json 配置文件
    - 添加多个对齐相关的 SVG 图标- 添加后退图标

---
+++ tailwind.config.ts
@@ -0,0 +1,57 @@
+import type { Config } from "tailwindcss";
+
+const config = {
+    content: [
+        "./pages/**/*.{ts,tsx}",
+        "./components/**/*.{ts,tsx}",
+        "./app/**/*.{ts,tsx}",
+        "./src/**/*.{ts,tsx}",
+    ],
+    prefix: "",
+    theme: {
+        container: {
+            center: true,
+            padding: "2rem",
+            screens: {
+                "2xl": "1400px",
+            },
+        },
+        extend: {
+            colors: {
+                primary: {
+                    black: "#14181F",
+                    green: "#56FFA6",
+                    grey: {
+                        100: "#2B303B",
+                        200: "#202731",
+                        300: "#C4D3ED",
+                    },
+                },
+            },
+            ringColor: {
+                'primary-green': '#56FFA6',
+                'primary-black': '#14181F',
+                'primary-gray-100': '#2B303B',
+                'primary-gray-200': '#202731',
+                'primary-gray-300': '#C4D3ED',
+            },
+            keyframes: {
+                "accordion-down": {
+                    from: { height: "0" },
+                    to: { height: "var(--radix-accordion-content-height)" },
+                },
+                "accordion-up": {
+                    from: { height: "var(--radix-accordion-content-height)" },
+                    to: { height: "0" },
+                },
+            },
+            animation: {
+                "accordion-down": "accordion-down 0.2s ease-out",
+                "accordion-up": "accordion-up 0.2s ease-out",
+            },
+        },
+    },
+    plugins: [require("tailwindcss-animate")],
+} satisfies Config;
+
+export default config;
